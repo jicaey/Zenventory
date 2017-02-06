a77c6e8a1f148711dbccb076a05ab69d0e7cde2e
jicaey <michaelyoung.dev@gmail.com>
2017-02-06 16:41:51 -0500

    Removes CollectionViewContoller Class and Object

--- Zenventory/DataStore.swift
+++ Zenventory/DataStore.swift
@@ -13,4 +13,19 @@ class DataStore {
     private init() {}
     
     var inventory = [Item]()
+    
+    func generateTestData() {
+        let pen: Item = EverydayCarry(name: "Fisher Space Pen", notes: "Raw Brass")
+        let shirt: Item = Wardrobe(name: "Sweater", notes: "Blue Cashmere")
+        let shoes: Item = Wardrobe(name: "Winter Boots", notes: "Grey Redwings")
+        let incense: Item = Consumables(name: "Nagchampa Incense", notes: "Incense Cones")
+        let macbook: Item = Electronics(name: "Macbook Air", notes: "11-inch, Early 2015")
+        let scissors: Item = Core(name: "Scissors", notes: "Clauss Titanium")
+        
+        inventory.append(contentsOf: [pen, shirt, shoes, incense, macbook, scissors])
+        print(" Inventory Array: \(inventory)")
+        
+        
+    }
+
 }
