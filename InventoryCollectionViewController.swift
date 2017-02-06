d703e177deb505b75ae9b2166c3eaa123c714437
jicaey <michaelyoung.dev@gmail.com>
2017-02-06 14:53:33 -0500

    Adds category structs

--- InventoryCollectionViewController.swift
+++ InventoryCollectionViewController.swift
@@ -15,20 +15,14 @@ class InventoryCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
         // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func generateTestData() {
+        let pen: Item = Item
     }
-
+    
     /*
     // MARK: - Navigation
 
--- Zenventory/Item.swift
+++ Zenventory/Item.swift
@@ -11,7 +11,57 @@ import Foundation
 protocol Item {
     var name: String { get }
     var description: String { get }
+    var category: Category { get }
+    
+    init()
+}
+
+struct Wardrobe {
+    var name: String
+    var description: String
     var category: Category
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+        self.category = Category.wardrobe
+    }
+}
+
+struct EverydayCarry {
+    var name: String
+    var description: String
+    var category: Category
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+        self.category = Category.everydayCarry
+    }
+}
+
+struct Core {
+    var name: String
+    var description: String
+    var category: Category
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+        self.category = Category.core
+    }
+}
+
+struct Consumables {
+    var name: String
+    var description: String
+    var category: Category
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+        self.category = Category.consumables
+    }
 }
 
 enum Category {
