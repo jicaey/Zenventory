ef835bcd3bb147833916fa0844431661f89153c8
jicaey <michaelyoung.dev@gmail.com>
2017-02-07 21:30:11 -0500

    Add constants file and color theme

---
+++ Zenventory/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Zenventory
+//
+//  Created by Michael Young on 2/7/17.
+//  Copyright © 2017 Michael Young. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    
+    struct Colors {
+        static let green = UIColor(red: 10, green: 57, blue: 41, alpha: 1)
+        static let darkBlue = UIColor(red: 79, green: 143, blue: 151, alpha: 1)
+        static let midBlue = UIColor(red: 149, green: 187, blue: 182, alpha: 1)
+        static let lightBlue = UIColor(red: 202, green: 217, blue: 205, alpha: 1)
+        static let whiteBlue = UIColor(red: 248, green: 238, blue: 212, alpha: 1)
+    }
+}
